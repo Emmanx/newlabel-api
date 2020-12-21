9f6b9b508b4321d0d8d38f38e0afda3b268d3276
Emmax <emmax@Emmaxs-MacBook-Pro.local>
2020-12-21 16:00:26 +0100

    feat: 🎸 Initital Strapi Setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9eaa89a785891e1123e126c4d12dfc3b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ea9808be-79ec-487b-becd-7b67996e0b14'
+};
