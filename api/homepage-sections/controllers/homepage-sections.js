ca233f3ff957ad94f2d475a19664f8b2e540c00b
Emmax <emmax@Emmaxs-MacBook-Pro.local>
2020-12-22 16:36:39 +0100

    feat: 🎸 Modified homepage-sections controller to return genres

--- api/homepage-sections/controllers/homepage-sections.js
+++ api/homepage-sections/controllers/homepage-sections.js
@@ -5,4 +5,24 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services["homepage-sections"].search(ctx.query);
+    } else {
+      entities = await strapi.services["homepage-sections"].find(ctx.query, [
+        {
+          path: 'products',
+          populate: {
+            path: 'genres'
+          }
+        }
+      ]);
+    }
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models["homepage-sections"] }));
+  },
+};
