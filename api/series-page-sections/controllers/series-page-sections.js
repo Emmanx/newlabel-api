85bba39351b7eda210162dcabf47c00046bddefe
Emmax <emmax@Emmaxs-MacBook-Pro.local>
2021-01-11 06:20:51 +0100

    feat: 🎸 Modified find methods page sections to include genres

--- api/series-page-sections/controllers/series-page-sections.js
+++ api/series-page-sections/controllers/series-page-sections.js
@@ -5,4 +5,22 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services["series-page-sections"].search(ctx.query);
+    } else {
+      entities = await strapi.services["series-page-sections"].find(ctx.query, [
+        {
+          path: 'products',
+          populate: {
+            path: 'genres'
+          }
+        }
+      ]);
+    }
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models["series-page-sections"] }));
+  },
+};
